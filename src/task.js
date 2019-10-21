7abfa70cea8ed583d866584fd39db84490bc1ef0
v.vayavodzin <v.vayavodzin@godeltech.com>
2019-10-21 15:35:44 +0300

    feat: create task id;

--- src/task.js
+++ src/task.js
@@ -1,6 +1,7 @@
 export class Task {
     constructor(title) {
       this.title = title;
+      this.id = Math.floor(Math.random() * 10000);
       this.status = 0;
       this.timestamp = Date.now();
     }
