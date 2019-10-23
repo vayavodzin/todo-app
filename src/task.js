31d4736d8a0ab49f77fd60249b728d67c16436d7
v.vayavodzin <v.vayavodzin@godeltech.com>
2019-10-23 16:09:49 +0300

    fix: make Task properties protected;

--- src/task.js
+++ src/task.js
@@ -1,13 +1,30 @@
+"use strict";
 export class Task {
-    constructor(title) {
-      this.title = title;
-      this.id = Math.floor(Math.random() * 10000);
-      this.status = 0;
-      this.timestamp = Date.now();
-    }
-  
-    toggle() {
-      this.status = this.status ? 0 : 1;
-    }
-  
+  constructor(title) {
+    this._title = title;
+    this._id = Math.floor(Math.random() * 10000);
+    this._status = 0;
+    this._timestamp = Date.now();
   }
+
+  get title() {
+    return this._title;
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get status() {
+    return this._status;
+  }
+
+  get timestamp() {
+    return this._timestamp;
+  }
+
+  toggle() {
+    this._status = this.status ? 0 : 1;
+  }
+
+}
